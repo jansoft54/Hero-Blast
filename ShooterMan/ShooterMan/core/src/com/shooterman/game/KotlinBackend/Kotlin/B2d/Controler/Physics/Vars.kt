03fa3ca8d685342c675d541fa4ba629d6e032b11
jansoft54 <Hdt.tec@gmail.com>
2017-08-04 21:21:41 +0200

    delete

--- ShooterMan/ShooterMan/core/src/com/shooterman/game/KotlinBackend/Kotlin/B2d/Controler/Physics/Vars.kt
+++
@@ -1,8 +0,0 @@
-package com.shooterman.game.KotlinBackend.Kotlin.B2d.Controler.Physics
-
- class Vars{
-     companion object {
-         @JvmField
-         val PPM=100
-     }
- }
--- ShooterMan/core/src/com/shooterman/game/Things/LevelManager.java
+++
@@ -1,8 +0,0 @@
-package com.shooterman.game.Things;
-
-/**
- * Created by master on 20.11.2016.
- */
-
-public class LevelManager {
-}
