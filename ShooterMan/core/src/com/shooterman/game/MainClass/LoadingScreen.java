386962e4a367f22c51f7f58fe2b4d566913f91ae
jansoft54 <Hdt.tec@gmail.com>
2017-08-06 23:45:11 +0200

    Laoding Screen

--- ShooterMan/core/src/com/shooterman/game/MainClass/LoadingScreen.java
+++ ShooterMan/core/src/com/shooterman/game/MainClass/LoadingScreen.java
@@ -3,16 +3,20 @@ package com.shooterman.game.MainClass;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.shooterman.game.KotlinBackend.Kotlin.Assets.AssetsManager;
 
 
 public class LoadingScreen implements Screen {
 
+    private ShapeRenderer shapeRenderer;
+    private OrthographicCamera camera;
 
-
-    public LoadingScreen() {
-
+    LoadingScreen() {
         AssetsManager.Manager.load();
+        shapeRenderer = new ShapeRenderer();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, ShooterMain.WIDTH, ShooterMain.HEIGHT);
     }
 
     @Override
@@ -21,12 +25,20 @@ public class LoadingScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        if (!AssetsManager.Manager.getManager().update())
-            System.out.println(AssetsManager.Manager.getManager().getProgress() * 100);
-        else {
-            OrthographicCamera camera = new OrthographicCamera();
-          ShooterMain.shootermain.setScreen(new PlayScreen( new SpriteBatch(),camera));
-        }
+
+        if (!AssetsManager.Manager.getManager().update()) {
+
+            float progress = AssetsManager.Manager.getManager().getProgress();
+
+            shapeRenderer.setProjectionMatrix(camera.combined);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.setColor(37 / 255f, 79 / 255f, 58 / 255f, 1);
+            shapeRenderer.rect(0, ShooterMain.HEIGHT / 2, ShooterMain.WIDTH * progress, ShooterMain.HEIGHT * 0.01f);
+            shapeRenderer.end();
+            System.out.println(progress * 100);
+
+        } else
+            ShooterMain.shootermain.setScreen(new PlayScreen(new SpriteBatch(), camera));
     }
 
     @Override
