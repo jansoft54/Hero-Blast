ef5146c4e88d9da45a1175f143f939a63371b97d
jansoft54 <Hdt.tec@gmail.com>
2017-08-07 17:00:22 +0200

    ssh Key test

--- ShooterMan/core/src/com/shooterman/game/Component/HealthComponent.java
+++ ShooterMan/core/src/com/shooterman/game/Component/HealthComponent.java
@@ -1,18 +1,24 @@
 package com.shooterman.game.Component;
 
+import com.shooterman.game.Component.Entity.Entity;
 import com.shooterman.game.Component.IComponent.IComponent;
 
 
 public class HealthComponent implements IComponent {
     private float health;
+    private Entity myEnity;
 
-    public HealthComponent(float health) {
+    public HealthComponent(Entity myEnity,float health) {
         this.health = health;
+        this.myEnity = myEnity;
     }
 
     @Override
     public void update(float delta) {
-    System.out.println("HIsd");
+
+    }
+    public void takeDamage(float damage){
+        health-= damage;
     }
 
 
